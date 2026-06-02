188b0edbe342108670df136f7f79c3779366b55c
Pamela-Epperson <60546246+Pamela-Epperson@users.noreply.github.com>
2026-06-02 15:52:57 -0400

    Create main.jsx
    main src file

---
+++ src/main.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ImpactMap from './ImpactMap'
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><ImpactMap /></React.StrictMode>)
